31de40dbf6a6af6d6a8e71860097d20a27cf4295
dingdongg <cs.dg.kim@gmail.com>
2023-04-02 10:02:54 -0700

    fix color scheme issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -59,7 +59,7 @@ function RootLayoutNav() {
       tokenCache={tokenCache}
     >
       <SignedIn>
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <ThemeProvider value={DefaultTheme}>
           <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
