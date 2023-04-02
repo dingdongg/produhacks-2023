23a621d66286c3f9ccf7855115f3bd0c746d8b38
dingdongg <cs.dg.kim@gmail.com>
2023-04-02 09:08:49 -0700

    add shadow effects

--- components/MetricCard.tsx
+++ components/MetricCard.tsx
@@ -1,16 +1,13 @@
-// import { View, Text } from "../components/Themed";
-import { View, Text } from "react-native";
-import MaskedView from "@react-native-masked-view/masked-view";
-import { LinearGradient } from "expo-linear-gradient";
+import { View, Text, TextProps } from "react-native";
 import { StyleSheet } from "react-native";
 
 export default function MetricCard({ metric, value }: { metric: string, value: number }) {
     return (
         <View style={styles.metricCardContainer}>
             <View>
-                <Text style={{ fontSize: 16, color: "orange" }}>{ value }</Text>
+                <Text style={{ color: "green", fontSize: 32 }}>{ value }</Text>
             </View>
-            <Text style={{ fontSize: 16, color: "red" }} onPress={() => alert("pressed")}>{ metric }</Text>
+            <Text style={{ fontSize: 16, color: "black" }} onPress={() => alert("pressed")}>{ metric }</Text>
         </View>
     );
 }
@@ -21,11 +18,18 @@ const styles = StyleSheet.create({
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        borderColor: "blue",
-        borderWidth: 2,
+        borderWidth: 0,
         width: "45%",
         height: "50%",
         margin: 5,
         borderRadius: 15,
+        shadowColor: "#000000",
+        shadowOffset: {
+            width: 0,
+            height: 6,
+        },
+        shadowOpacity:  0.21,
+        shadowRadius: 6.65,
+        elevation: 9
     },
 });
