051579b36acbb6988d1b796c890fba2e5e729860
dingdongg <cs.dg.kim@gmail.com>
2023-04-02 05:34:20 -0700

    stub profile component

---
+++ app/profile.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Profile() {
+    return (
+        <Text style={{ color: "black" }}>
+            your profile :o
+        </Text>
+    )
+}
