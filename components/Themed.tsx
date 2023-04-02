61ab476d019c05cf04a84c359060ab2258362898
dingdongg <cs.dg.kim@gmail.com>
2023-04-02 03:55:37 -0700

    redesign SafeView

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -3,7 +3,12 @@
  * https://docs.expo.io/guides/color-schemes/
  */
 
-import { Text as DefaultText, useColorScheme, View as DefaultView } from 'react-native';
+import { 
+  Text as DefaultText, 
+  useColorScheme, 
+  View as DefaultView,
+  SafeAreaView as DefaultSafeView 
+} from 'react-native';
 
 import Colors from '../constants/Colors';
 
@@ -28,6 +33,7 @@ type ThemeProps = {
 
 export type TextProps = ThemeProps & DefaultText['props'];
 export type ViewProps = ThemeProps & DefaultView['props'];
+export type SafeAreaViewProps = ThemeProps & DefaultSafeView['props'];
 
 export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
@@ -41,4 +47,12 @@ export function View(props: ViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
   return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+}
+
+// iOS only.
+export function SafeView(props: SafeAreaViewProps) {
+  const { style, lightColor, darkColor, ...otherProps } = props;
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+
+  return <DefaultSafeView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
