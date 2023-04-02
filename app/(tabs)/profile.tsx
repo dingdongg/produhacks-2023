64fdda45f1a4b179d73fc748fd6ee415fadb0337
dingdongg <cs.dg.kim@gmail.com>
2023-04-02 09:07:03 -0700

    render profile buttons

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,68 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { useClerk } from "@clerk/clerk-expo";
+import { LinearGradient } from "expo-linear-gradient";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons'; 
+import { MaterialIcons } from '@expo/vector-icons'; 
 
 export default function Profile() {
+    const { signOut } = useClerk();
+
     return (
         <View style={styles.profileContainer}>
-            <Text style={{ color: "black" }}>
-                your profile :o
-            </Text>
+            <LinearGradient
+                    colors={['#92C5EB', '#FFBBE8']}
+                    start={{ x: 0, y: 1 }}
+                    end={{ x: 1, y: 1 }}
+                    style={styles.loginGradientContainer}
+                >
+                <TouchableOpacity onPress={() => signOut()} style={styles.profileButtons}>
+                    <Ionicons name="ios-exit-outline" size={24} color="white" />
+                    <Text style={[styles.loginButton]}>
+                        Sign Out
+                    </Text>
+                </TouchableOpacity>
+            </LinearGradient>
+            <LinearGradient
+                    colors={['#92C5EB', '#FFBBE8']}
+                    start={{ x: 0, y: 1 }}
+                    end={{ x: 1, y: 1 }}
+                    style={styles.loginGradientContainer}
+                >
+                <TouchableOpacity style={styles.profileButtons}>
+                    <Ionicons name="ios-settings-outline" size={24} color="white" />
+                    <Text style={[styles.loginButton, { textAlign: "center" }]}>
+                        Settings
+                    </Text>
+                </TouchableOpacity>
+            </LinearGradient>
+            <LinearGradient
+                    colors={['#92C5EB', '#FFBBE8']}
+                    start={{ x: 0, y: 1 }}
+                    end={{ x: 1, y: 1 }}
+                    style={styles.loginGradientContainer}
+                >
+                <TouchableOpacity style={styles.profileButtons}>
+                    <AntDesign name="staro" size={24} color="white" />
+                    <Text style={[styles.loginButton, { textAlign: "center" }]}>
+                        Subscription
+                    </Text>
+                </TouchableOpacity>
+            </LinearGradient>
+            <LinearGradient
+                    colors={['#92C5EB', '#FFBBE8']}
+                    start={{ x: 0, y: 1 }}
+                    end={{ x: 1, y: 1 }}
+                    style={styles.loginGradientContainer}
+                >
+                <TouchableOpacity style={styles.profileButtons}>
+                    <MaterialIcons name="contact-support" size={24} color="white" />
+                    <Text style={[styles.loginButton, { textAlign: "center" }]}>
+                        Support
+                    </Text>
+                </TouchableOpacity>
+            </LinearGradient>
         </View>
     );
 }
@@ -17,5 +74,30 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "white",
+    },
+    loginButton: {
+        paddingLeft: 15,
+        paddingRight: 20,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderRadius: 15,
+        fontWeight: "600",
+        color: "white",
+    },
+    loginGradientContainer: { 
+        borderRadius: 15, 
+        marginTop: 10, 
+        width: "60%",
+        shadowOffset: {
+            height: -49,
+            width: 0,
+        },
+        shadowRadius: 10,
+    },
+    profileButtons: { 
+        display: "flex", 
+        flexDirection: "row", 
+        justifyContent: "center", 
+        alignItems: "center",
     },
 });
